388e9bb3070a5e7ae2ff402f4a9135563a52afb5
Saeed Matar <eng.saeed.matar@gmail.com>
2017-05-20 21:18:31 +0300

    remove seeder and update config file

--- src/config/settings.php
+++ src/config/settings.php
@@ -1,6 +1,22 @@
 <?php
 
 return [
+    //settings route
     'route' => 'settings',
-    'middleware' => ['web', 'auth']
+
+    'middleware' => ['web', 'auth'],
+
+    // hidden records not editable from interface when set to false
+    'show_hidden_records' => false,
+
+    //javascript format
+    'date_format' => 'mm/dd/yyyy',
+    // number of digits after the decimal point
+    'number_step' => 0.001,
+
+    // upload path for settings of type file
+    'upload_path' => 'uploads/settings',
+
+    // valid mime types for settings of type file
+    'mimes' => 'jpg,jpeg,png,txt,csv,pdf',
 ];
